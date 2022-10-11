94c1bf4bbbb6ad6eb1b78c7ddc78d2097a4af30e
Yunus <yunus89dogan@gmail.com>
2022-10-10 23:18:48 -0700

    For eklendi V1

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -2,6 +2,12 @@ package Gun01;
 
 public class Merhaba {
     public static void main(String[] args) {
+        System.out.println("Merhaba");
 
+        for (int i = 0; i <5 ; i++) {
+
+            System.out.println("Merhaba"+i);
+
+        }
     }
 }
